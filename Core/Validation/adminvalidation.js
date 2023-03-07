e95795f20bcdd5e64f78b299a85f52a71a7ca13b
MariamZayed <mariamzayed91@gmail.com>
2023-03-07 18:09:47 +0200

    Create admin validation

---
+++ Core/Validation/adminvalidation.js
@@ -0,0 +1,27 @@
+const { body, param, query } = require("express-validator");
+
+exports.post = [
+    body("firstName").isString().withMessage("First Name must be string"),
+    body("lastName").isString().withMessage("Last Name must be string"),
+    body("password").isLength({min: 8}).withMessage("Password Must be Min length 8"),
+    body("email").isEmail().withMessage("Invalid Email Format"),
+    body("birthdate").isDate().withMessage("Invalid Date"),
+    body("hireDate").isDate().withMessage("Invalid Hire Date"),
+    body("image").isString(to).withMessage(" Invalid Image"),
+    body("salary").isNumeric().withMessage("Salary Must be Number"),
+];
+
+exports.update = [
+    body("id").isMongoId().withMessage("Id Shoud be Object"),
+    body("lastName").optional().isString().withMessage("Last Name must be string"),
+    body("password").optional().isLength({min: 8}).withMessage("Password Must be Min length 8"),
+    body("email").optional().isEmail().withMessage("Invalid Email Format"),
+    body("birthdate").optional().isDate().withMessage("Invalid Date"),
+    body("hireDate").optional().isDate().withMessage("Invalid Hire Date"),
+    body("image").optional().isString(to).withMessage("Invalid Image"),
+    body("salary").optional().isNumeric().withMessage("Salary Must be Number")
+];
+
+exports.delete = [
+    body("id").isMongoId().withMessage("Id Shoud be Object"),
+]
