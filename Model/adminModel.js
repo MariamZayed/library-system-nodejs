cf8bf8214440f51961da1644b14cdd50d26e2f93
MariamZayed <mariamzayed91@gmail.com>
2023-03-07 17:20:01 +0200

    Complete admin model schema

--- Model/adminModel.js
+++ Model/adminModel.js
@@ -1,43 +1,41 @@
 const mongoose=require("mongoose")
 
 const Schema=new mongoose.Schema({
-    _id:{
-        type:Number
-    },
+    _id: { type:Number },
     firstName:{
-        type:String,
-        require:true
+        type: string,
+        validate: {
+            validator: function (v) {
+            return /^[a-zA-Z0-9 ]*$/.test(v);
+            },
+        message: "Please enter a valid name",
+        },
+        required: [true, "Title is required"],
     },
     lastName :{
-        type:String,
-        require:true
+        type: string,
+        required: [true, "Title is required"],
+        validate: {
+            validator: function (v) {
+            return /^[a-zA-Z0-9 ]*$/.test(v);
+            },
+            message: "Please enter a valid name",
+        },
     },
-    email:{
-        type:email
-        // ,
-        // validate:
-        
+    password: { type: String, required: true, length: {min: 8} },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "Email must be unique"],
+        trim: true,
+        lowercase: true,
+        match: [
+        /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/,
+        'Please fill a valid email address'
+        ],
     },
-    password:{
-        type:String
-        
-    },
-    birthdate:{
-        type:Date
-
-    },
-    hirdata:{
-        type:Date,
-        require:true
-
-    },
-    salary:{
-        type:Number,
-        require:true
-
-    },
-    image:{
-        type:Image
-    }
-
+    birthdate : { type:Date },
+    hireDate : { type:Date, require:true },
+    image : { type:Image },
+    salary : { type:Number, require:true }
 })
