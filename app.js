398036d04515e80a5ba4f29cc3e47f5f119805be
MariamZayed <mariamzayed91@gmail.com>
2023-03-08 07:22:59 +0200

    Refator app file

--- app.js
+++ app.js
@@ -9,34 +9,21 @@ const port = process.env.PORT || 8080;
 
 mongoose.set("strictQuery",false)
 
-// const start = async()=>{
-//     try{
-//         await mongoose.connect('mongodb+srv://nodejs:antilotfi43@librarynodejs.ym4zs66.mongodb.net/?retryWrites=true&w=majority');
-//         console.log("database connected");
-//         app.listen(port,()=>{
-//             console.log("server connected....");
-//         })
-//     } catch(e){
-//         console.log(e.message);
-//     }
-// };
-
-mongoose.connect('mongodb+srv://nodejs:antilotfi43@librarynodejs.ym4zs66.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => {
-        console.log("database connected");
+const start = async()=>{
+    try{
+        await mongoose.connect('mongodb+srv://nodejs:q7GOqqPWdQlbkaHH@librarynodejs.ym4zs66.mongodb.net/?retryWrites=true&w=majority');
+        console.log("Database Connected...");
         app.listen(port,()=>{
-            console.log("server connected....");
+            console.log("server is listenng... ",port);
         })
-    })
-    .catch((error)=> console.log(`DB connection error ${error}`))
-
+    } catch(e){
+        console.log(e.message);
+    }
+};
 
     app.use(cors());
     app.use(express.json());
     app.use(adminRoute);
-
-    
-
 
     app.use((request,response)=>{
         response.status(404).json({message:"Not Found"});
