1c276253dda931c521668fe1f529d38e29428f1c
Asmaa Abd ELhalem <120665113+asmaa388@users.noreply.github.com>
2023-03-17 19:51:33 +0200

    Update bookOperationController.js

--- Controller/bookOperationController.js
+++ Controller/bookOperationController.js
@@ -411,3 +411,62 @@ exports.getAllBookOperations = (request, response) => {
       next(error);
     });
 };
+
+
+// current borrowing books and return date 
+exports.getCurentBooksBorrow = (request, response, next) => {
+    //get current month from fist day to last day of month
+    const date = new Date();
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let f = new Date(year, month, 1).getDate();
+    let l = new Date(year, month, 0).getDate();
+    //format
+    f = f < 10 ? '0'+f : f;
+    l = l < 10 ? '0'+l : l;
+    month = month < 10 ? '0'+month : month;
+  
+    const firstDay = new Date(`${year}-${month}-${f}`);
+    const lastDay = new Date(`${year}-${month}-${l}`);
+    //console.log(firstDay,lastDay)
+    bookOperattion.aggregate([ 
+    {
+        $match:
+        {
+            operationOnBook:{$eq:"borrowing"},
+            startOperation:{
+              $gte :new Date (`${firstDay}`),
+              $lte:new Date (`${lastDay}`)
+            },
+            isReturn:{$eq:"false"},
+            //memberId:{$eq:request.id}
+        }
+    }// stage1
+    ,
+    {
+      $lookup: {
+        from: "books",
+        localField: "bookId",
+        foreignField: "_id",
+        as: "borrowBooks"
+    }
+    },
+    {
+   $project:
+        {
+              startOperation:1,
+              return:1,
+              operationOnBook:1,
+              memberId:1,
+              borrowBooks:"$borrowBooks"
+        }
+    }// stage2
+    
+  ])
+  .then((data) => {
+        response.status(200).json({ data });
+      })
+      .catch((error) => next(error));
+  
+  
+}
