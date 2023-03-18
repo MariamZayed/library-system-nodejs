e8e2645cf90fe278796d52708920b25f7afac30f
MariamZayed <mariamzayed91@gmail.com>
2023-03-18 15:25:40 +0200

    Add new cruds to book controller Refactor delete admin to del image

--- Routes/bookRoute.js
+++ Routes/bookRoute.js
@@ -10,11 +10,26 @@ router
   .post(bookValidation.post, validateMW, controller.addBook)
   .patch(bookValidation.update, validateMW, controller.updateBook)
   .delete(bookValidation.delete, validateMW, controller.deleteBook);
-router.route("/book/ArrivalBook").get(controller.getArrivalBook);
+router
+  .route("/book/ArrivalBook")
+  .get(controller.getArrivalBook);
+router
+  .route("/book/arrivalbookbyyear")
+  .get(controller.getNewArrivalBooks)
+router
+  .route("/book/avilableBook")
+  .get(controller.getAvailableBooks)
+router
+  .route("/book/mostBorrowedBooks")
+  .get(controller.mostBorrowedBooks)
+router
+  .route("/book/mostReadBooks")
+  .get(controller.mostReadBooks)
 router
   .route("/book/searchBookByYear/:year")
   .get(controller.searchBookByYear)
   
-router.get("/book/:id", controller.getOneBook);
+router
+  .get("/book/:id", controller.getOneBook);
 
 module.exports = router;
