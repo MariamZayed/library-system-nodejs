0d9799028ced8b6fc956efb8699907c986c18a5e
maas97 <mashraf.14697@gmail.com>
2023-03-18 12:35:15 +0200

    add reports

--- Controller/reportsController.js
+++ Controller/reportsController.js
@@ -1,67 +1,78 @@
+const { request, response } = require("express");
 const mongoose = require("mongoose");
+let fs = require("fs")
 require("./../Model/BookModel");
 require("./../Model/BookOperation");
 require("./../Model/adminModel");
 require("./../Model/memberModel");
 require("./../Model/employeeModel");
+let operations = require("./../Controller/bookOperationController");
 //getter
-const adminSchema = mongoose.model("admins","system");
+const adminSchema = mongoose.model("admins");
 const employeeSchema = mongoose.model("employees");
 const memberSchema = mongoose.model("member");
 const bookSchema = mongoose.model("book");
 const bookOperationSchema = mongoose.model("bookOperattion");
 
+
+
 exports.getBooksCounts = (request, response, next) => {
-    bookSchema
-      .estimatedDocumentCount()
-      .then((data) => {
-        response.status(200).json({ "Number of books in the system" : data });
-      })
-      .catch((error) => {
-        next(error);
-      });
-  };
+  bookSchema
+    .estimatedDocumentCount()
+    .then((data) => {
+      response.status(200).json({ "Number of books in the system": data });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
 
-  exports.getAdminCounts = (request, response, next) => {
-    adminSchema
-      .estimatedDocumentCount()
-      .then((data) => {
-        response.status(200).json({ "Number of admins in the system" : data });
-      })
-      .catch((error) => {
-        next(error);
-      });
-  };
+exports.getAdminCounts = (request, response, next) => {
+  adminSchema
+    .estimatedDocumentCount()
+    .then((data) => {
+      response.status(200).json({ "Number of admins in the system": data });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
 
-  exports.getEmployeeCounts = (request, response, next) => {
-    employeeSchema
-      .estimatedDocumentCount()
-      .then((data) => {
-        response.status(200).json({ "Number of employees in the system" : data });
-      })
-      .catch((error) => {
-        next(error);
-      });
-  };
+exports.getEmployeeCounts = (request, response, next) => {
+  employeeSchema
+    .estimatedDocumentCount()
+    .then((data) => {
+      response.status(200).json({ "Number of employees in the system": data });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
 
-  exports.getMemberCounts = (request, response, next) => {
-    bookSchema
-      .estimatedDocumentCount()
-      .then((data) => {
-        response.status(200).json({ "Number of members in the system" : data });
-      })
-      .catch((error) => {
-        next(error);
-      });
-  };
+exports.getMemberCounts = (request, response, next) => {
+  bookSchema
+    .estimatedDocumentCount()
+    .then((data) => {
+      response.status(200).json({ "Number of members in the system": data });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
 
-  exports.getBooksCounts = (request, response, next) => {
-    bookSchema
-      .estimatedDocumentCount()
-      .then((data) => {
-        response.status(200).json({ "Number of books in the system" : data });
-      })
-      .catch((error) => {
-        next(error);
-      });
-  };
+exports.getBooksCounts = (request, response, next) => {
+  bookSchema
+    .estimatedDocumentCount()
+    .then((data) => {
+      response.status(200).json({ "Number of books in the system": data });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+exports.getBookOperations = (request, response, next) => {
+  let report = {
+    [operations.getBooksBorrow,operations.getBooksreading,operations.]
+  }
+};
--- Routes/reportsRoute.js
+++ Routes/reportsRoute.js
@@ -7,4 +7,6 @@ router.route("/counterempreport").get(controller.getEmployeeCounts);
 router.route("/countermemberreport").get(controller.getMemberCounts);
 router.route("/counterbookreport").get(controller.getBooksCounts);
 
+// router.route("/bookreports").get(controller);
+
 module.exports = router;
